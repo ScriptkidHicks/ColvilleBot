9333ebd821c5aa89f85b7a97693c2da328d57252
Tammashicks <tammas@uoregon.edu>
2021-01-23 20:40:23 -0800

    This commit adds the basic functionality of the initiative tracker for the bot. Current prefix is 'Colville '

--- ColvilleBot.py
+++ ColvilleBot.py
@@ -1,12 +1,13 @@
 """
 This is the first version of the Colville Bot
 """
+import random
 import discord
 import asyncio
 from discord.ext import commands
 
 
-TOKEN = "Your token goes here"
+TOKEN = "Your Token goes here"
 intents = discord.Intents.default()
 intents.members = True
 
@@ -20,6 +21,12 @@ class Initiative:
     def intake(self, member: str, bonus: int):
         self.members[member] = bonus
 
+    def insert(self, member: str, bonus: int):
+        self.members[member] = bonus
+        roll = random.randint(1, 20) + bonus
+        self.order.append((member, roll))
+        self.order.sort(reverse=True, key=lambda tup: tup[1])
+
     def remove(self, member: str):
         try:
             self.members.pop(member)
@@ -27,15 +34,66 @@ class Initiative:
         except KeyError:
             return f"{member} was not in initiative."
 
+    def rollup_initiative(self):
+        self.order = []
+        for member in self.members:
+            roll = random.randint(1, 20)
+            self.order.append((member, self.members[member] + roll))
+            self.order.sort(reverse=True, key=lambda tup: tup[1])
 
-initiative_roll = Initiative()
+    def clear(self):
+        self.order = []
+        self.members = {}
 
-Colville = commands.Bot(command_prefix='>')
+    def present(self):
+        send_string = ""
+        for member in self.order:
+            send_string += f"{member[0]}: {member[1]}\n"
+        return send_string
+
+
+class colvile(commands.Bot):
+
+    def __init__(self):
+        super().__init__(command_prefix='Colville ')
+        self.Initiative = Initiative()
+
+
+Colville = colvile()
 
 
 @Colville.event
 async def on_ready():
     print("This bot has connected to the server")
+
+
+@Colville.command()
+async def RollInit(ctx):
+    Colville.Initiative.rollup_initiative()
+    sendout = Colville.Initiative.present()
+    if sendout == '':
+        await ctx.send("There are currently no players in combat")
+    else:
+        await ctx.send(sendout)
+
+
+@Colville.command()
+async def NewJoin(ctx, member, bonus):
+    Colville.Initiative.insert(member, int(bonus))
+    ctx.send(Colville.Initiative.present())
+
+
+@Colville.command()
+async def NewMember(ctx, member, bonus):
+    Colville.Initiative.intake(member, int(bonus))
+    await ctx.send(f"New combatant {member} with initiative bonus {bonus}\n"
+             f"There are currently {len(Colville.Initiative.members)} combatants.")
+
+
+@Colville.command()
+async def PresentInit(ctx):
+    this = Colville.Initiative.present()
+    await ctx.send(this)
 
 
 @Colville.command()
@@ -52,3 +110,4 @@ async def test(ctx):
                    "life. God keep thee! Push not off from that isle, thou canst never return!")
 
 Colville.run(TOKEN)
+
